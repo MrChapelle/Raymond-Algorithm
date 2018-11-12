d7cc51f990d06f19d7676945712d87d6880352df
MrChapelle <robin.you@telecom-bretagne.eu>
2018-11-12 17:36:35 +0100

    WIP

--- src/__init__.py
+++ src/__init__.py
@@ -6,12 +6,6 @@ def init_system():
 	"""
 	Function which create the initial state of the graph
 	"""
-	Node_0 = Node(0, None, True)
-	Node_1 = Node(1, 0)
-	Node_2 = Node(2, 0)
-	Node_3 = Node(3, 2)
-	Node_4 = Node(4, 3)
-	Node_5 = Node(5, 3)
 
 	Graph = [Node_0, Node_1, Node_2, Node_3, Node_4, Node_5]
 
@@ -19,4 +13,14 @@ def init_system():
 		node.init_queue()
 
 if __name__ == '__main__':
+
+	# Graph Creation
+	Node_0 = Node(0, 0, [], True)
+	Node_1 = Node(1, 0)
+	Node_2 = Node(2, 0)
+	Node_3 = Node(3, 2)
+	Node_4 = Node(4, 3)
+	Node_5 = Node(5, 3)
+
+	#Queue Initialization
 	init_system()
--- src/node.py
+++ src/node.py
@@ -1,11 +1,12 @@
 import pika
 
 class Node():
-	def __init__(self, number, holder, queue = [], using = False):
+	def __init__(self, number, holder, queue = [], using = False, asked = False):
 		self.number = number
 		self.holder = holder
 		self.queue  = queue
 		self.using  = using 
+		self.asked  = asked
 
 #Getters -------------------------------------------------------
 	def get_number(self):
@@ -20,6 +21,19 @@ class Node():
 	def is_using(self):
 		return self.using
 #Methods -------------------------------------------------------
+	
+	def start_using(self):
+		if self.number == self.holder :
+			self.using = True
+		else :
+			print("You are not the holder, you can't be in critical section")
+
+	def stop_using(self):
+		if self.using and (self.number == self.holder):
+			self.using = False
+		else :
+			print("You are not in critical section because you are not the root or not using the token")	
+			
 	def init_queue(self):
 		connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 		channel    = connection.channel()
